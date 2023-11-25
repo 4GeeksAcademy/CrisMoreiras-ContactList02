b277db1dc28aac91183306a36b7bb51777bdcd1b
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-11-25 23:06:59 +0000

    05

--- src/js/layout.js
+++ src/js/layout.js
@@ -18,6 +18,7 @@ import { Footer } from "./component/footer";
 
 
 
+
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
--- src/js/views/contactlist.js
+++ src/js/views/contactlist.js
@@ -2,14 +2,14 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import {Contact} from "./views/contact";
+
 
 
 
 export const ContactList = () => {
 	return (
         <>
-		<Contact />
+		<h1>hello</h1>
         </>
 	);
 };
